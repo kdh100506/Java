19eb341a52a04ba060403950b13dcc4d5ab75155
kdh100506 <kdh100506@dgsw.hs.kr>
2026-09-04 11:03:45 +0900

    commit

---
+++ src/array/ArrayTest.java
@@ -0,0 +1,13 @@
+package array;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] num = new int[] {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("배열의 길이 : " + num.length);
+        System.out.println(num[0]);
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
