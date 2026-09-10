90fb2799bcb8ef0c338864a8cf42722c394bab2c
kdh100506 <kdh100506@dgsw.hs.kr>
2026-09-10 15:37:42 +0900

    commit

---
+++ src/array/Quiz.java
@@ -0,0 +1,13 @@
+package array;
+
+public class Quiz {
+    public static void main(String[] args) {
+        // int[] arr1 = new int[5]{10, 20, 30, 40, 50}; --- 값을 초기화할때에는 크기를 지정하면 안됨
+
+        // int [] arr2 = new int[]; --- new를 쓰면 []에 값을 넣어야 함
+
+         int arr3[] = new int[4];
+
+        // int arr4 = new int[3]; --- arr4뒤에 []가 빠짐
+    }
+}
---
+++ src/classpart/Student.java
@@ -0,0 +1,23 @@
+package classpart;
+
+public class Student {
+    int studentID;
+    String studentName;
+    int grade;
+    String address;
+
+    public void showStudentInfo() {
+        System.out.println(studentName + " " + address);
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        studentLee.studentName = "이순신";
+        System.out.println(studentLee.studentName);
+        System.out.println(studentLee.getStudentName());
+    }
+}
